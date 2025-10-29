95a9a1693a03490ce098b12f0fb148e3bc894ae4
xLicht27 <138235405+xLicht27@users.noreply.github.com.>
2025-10-28 21:14:36 -0500

    Eliminando archivos no usados

--- src/main/java/com/springboot/sistema/hoteles/springboot_sistemahoteles/repositories/ProfesorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.sistema.hoteles.springboot_sistemahoteles.repositories;
-
-import com.springboot.sistema.hoteles.springboot_sistemahoteles.models.Profesor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfesorRepository extends JpaRepository<Profesor, Long> {
-}
